9d144e0b9e7ac9becd53192e8362025bbf684aae
Aishu030 <aishuramesh031@gmail.com>
2023-07-13 09:45:06 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //day04
+            //exercise 01
+            int pre, final;
+            Console.WriteLine("Enter pre mark");
+            pre = int.Parse(Console.ReadLine());
+         
+            if(pre >60)
+            {
+                Console.WriteLine("Enter marks in final");
+                final = int.Parse(Console.ReadLine());
+
+                if(final > 55)
+                {
+                    Console.WriteLine("Selected");
+                }
+                else
+                {
+                    Console.WriteLine("Fail in final");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fail in pre");
+            }
+            Console.ReadKey();
+        }
+    }
+}
